34d09dda9d836fbf4814d304724eb6b0cbc20f5f
arandriambololontsoa <andritiana.ran@gmail.com>
2017-12-06 00:14:38 +0100

    backgroud image cover

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage implements OnInit {
-
+export class HomePage implements AfterViewInit {
+  
+  @ViewChild('homeContent', { read: ElementRef }) 
+  private homeElem: ElementRef
+  
   constructor(public navCtrl: NavController) {
   }
-
-  public ngOnInit(): void {
+  
+  ngAfterViewInit(): void {
+    this.homeElem.nativeElement.style.background = 'url("https://source.unsplash.com/collection/1045802")';
+    this.homeElem.nativeElement.style.backgroundSize = 'cover';
   }
 }

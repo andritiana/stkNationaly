7c35323585bc098ba9f8dcbe5706e8afd0b6b0fc
arandriambololontsoa <andritiana.ran@gmail.com>
2017-12-07 00:35:29 +0100

    simplify background image

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,13 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage implements AfterViewInit {
-  
-  @ViewChild('homeContent', { read: ElementRef }) 
-  private homeElem: ElementRef
-  
+export class HomePage {
+
   constructor(public navCtrl: NavController) {
   }
-  
-  ngAfterViewInit(): void {
-    this.homeElem.nativeElement.style.background = 'url("https://source.unsplash.com/collection/1045802")';
-    this.homeElem.nativeElement.style.backgroundSize = 'cover';
-  }
+
 }

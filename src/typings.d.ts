759bcec8ed18e559f7fd86cfd0cf66b950735be9
hearoy <h.hary.r+gitlab@gmail.com>
2022-10-03 23:59:53 +0200

    docs: Declare used types of SC

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,8 +1,10 @@
 declare const ngDevMode: unknown;
-declare const SC: SC;
-
 // Declare manuellement la definition du widget de Soundcloud
 // afin que TypeScript ne lance pas d'erreur de type lorsqu'il voit SC.Widget
-interface SC {
-    Widget: any
+// https://developers.soundcloud.com/docs/api/html5-widget#api
+declare namespace SC {
+  function Widget(iframe: HTMLIFrameElement): Widget;
+  interface Widget {
+    pause(): void;
+  }
 }

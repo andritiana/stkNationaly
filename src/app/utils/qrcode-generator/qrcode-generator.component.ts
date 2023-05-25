cd5edc2c8f44da358b19b7045f7b99bb9fe03392
hearoy <h.hary.r+gitlab@gmail.com>
2023-05-25 13:51:20 +0200

    feat(profile): #143 stack multiple badges

--- src/app/utils/qrcode-generator/qrcode-generator.component.ts
+++ src/app/utils/qrcode-generator/qrcode-generator.component.ts
@@ -1,26 +1,38 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Inject, Input, SimpleChanges, ViewChild } from '@angular/core';
-import { toCanvas,  } from 'qrcode';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Inject,
+  Input,
+  SimpleChanges,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import { toCanvas } from 'qrcode';
+import * as color from 'color';
 import { WINDOW } from '../browser.service';
+import { Platform } from '@ionic/angular';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { startWith } from 'rxjs';
 
+@UntilDestroy()
 @Component({
   selector: 'mystk-qrcode-generator',
   templateUrl: './qrcode-generator.component.html',
-  styles: [':host { display: block; }'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styles: [':host { display: block; } canvas { width: 100%; }'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QrcodeGeneratorComponent {
   @Input()
   data: string = '';
-  @ViewChild('qrcodeCanvas', {static: true})
+  @ViewChild('qrcodeCanvas', { static: true })
   canvasRef?: ElementRef<HTMLCanvasElement>;
   private ctx?: CanvasRenderingContext2D | null = null;
   private color: string = '';
   private bgColor: string = '';
+  platform = inject(Platform);
 
-  constructor(
-    @Inject(WINDOW) private window: Window,
-    private eltRef: ElementRef<HTMLElement>,
-  ) { }
+  constructor(@Inject(WINDOW) private window: Window, private eltRef: ElementRef<HTMLElement>) {}
 
   ngOnInit(): void {
     const style = this.window.getComputedStyle(this.eltRef.nativeElement);
@@ -29,7 +41,7 @@ export class QrcodeGeneratorComponent {
 
     const canvas = this.canvasRef?.nativeElement;
     this.ctx = this.canvasRef?.nativeElement.getContext('2d');
-    this.drawQrCode(canvas);
+    this.platform.resize.pipe(startWith(void 0), untilDestroyed(this)).subscribe(() => this.drawQrCode(canvas));
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -41,7 +53,12 @@ export class QrcodeGeneratorComponent {
   private drawQrCode(canvas?: HTMLCanvasElement) {
     if (canvas && this.ctx) {
       this.ctx.clearRect(0, 0, canvas.width, canvas.height);
-      toCanvas(canvas, this.data, { color: { dark: this.color, light: this.bgColor }, errorCorrectionLevel: 'H', scale: 6 });
+      toCanvas(canvas, this.data, {
+        color: { dark: color(this.color).hex(), light: color(this.bgColor).hex() },
+        errorCorrectionLevel: 'H',
+        scale: 6,
+        margin: this.platform.width() > 375 ? 3 : 4,
+      });
     }
   }
 }

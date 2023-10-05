a647da9672338ef101781708027498b54c2b76c9
hearoy <h.hary.r+gitlab@gmail.com>
2023-10-05 19:12:51 +0200

    feat(event): Sort badges in dev mode

--- src/app/profile/badge-list/badge-list.module.ts
+++ src/app/profile/badge-list/badge-list.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { BadgeListRoutingModule } from './badge-list-routing.module';
 import { BadgeListPage } from './badge-list.page';
 import { IonicModule } from '@ionic/angular';
 import { GlobalHeaderModule } from 'src/app/global-header/global-header.module';
 import { BadgeCardModule } from '../badge-card/badge-card.module';
+import { LetModule } from '@rx-angular/template/let';
 
 
 @NgModule({
@@ -15,7 +17,9 @@ import { BadgeCardModule } from '../badge-card/badge-card.module';
     IonicModule,
     GlobalHeaderModule,
     BadgeCardModule,
-    BadgeListRoutingModule
+    BadgeListRoutingModule,
+    DragDropModule,
+    LetModule,
   ]
 })
 export class BadgeListModule { }

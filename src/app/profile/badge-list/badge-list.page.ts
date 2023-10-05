0b96e8f1426931fc10d7a418a475f0c2ef9febf8
hearoy <h.hary.r+gitlab@gmail.com>
2023-10-05 19:12:51 +0200

    feat(event): Sort badges in dev mode

--- src/app/profile/badge-list/badge-list.page.ts
+++ src/app/profile/badge-list/badge-list.page.ts
@@ -1,9 +1,13 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import type { OnInit } from '@angular/core';
+import { Component, HostBinding, inject } from '@angular/core';
+import type { Observable} from 'rxjs';
+import { Subject, combineLatest, map, shareReplay, startWith } from 'rxjs';
 
+import type { CdkDragDrop} from '@angular/cdk/drag-drop';
+import { moveItemInArray } from '@angular/cdk/drag-drop';
 import type { Badge, Profile } from '../profile.model';
 import { ProfileService } from '../profile.service';
-import { compareDesc, parseISO } from 'date-fns/esm';
+import { FpmaApiService } from 'src/app/services/fpma-api.service';
 
 @Component({
   selector: 'mystk-badge-list-page',
@@ -11,8 +15,12 @@ import { compareDesc, parseISO } from 'date-fns/esm';
   styleUrls: ['./badge-list.page.scss'],
 })
 export class BadgeListPage implements OnInit {
-  badgeList$!: Observable<(Badge & { transform: string })[]>;
+  badgeList$!: Observable<Badge[]>;
   profile$!: Observable<Profile>;
+  transforms$?: Observable<string[]>;
+  drop$ = new Subject<CdkDragDrop<Badge>>();
+  isDevMode$ = inject(FpmaApiService).isDevMode$;
+
   @HostBinding('style.--badge-event-height')
   badgeHeaderHeight = '4.125rem';
 
@@ -22,16 +30,31 @@ export class BadgeListPage implements OnInit {
 
   ngOnInit(): void {
     this.profile$ = this.profileService.getMyProfile();
-    this.badgeList$ = this.profileService.getMyBadges().pipe(
-      map((badges) => {
-        const size = badges.length;
-        return badges
-          .map((b, idx) => {
-            const translateY = idx < size - 1 ? (this.halfHeaderHeight * idx).toString() + 'px' : '0px';
-            const translateZ = (-5 * (size - idx - 1) + 1).toString() + 'px';
-            return { ...b, transform: `translateZ(${translateZ}) translateY(${translateY})` };
-          });
+    this.badgeList$ = combineLatest([
+      this.profileService.getMyBadges(),
+      this.drop$.pipe(startWith(null)),
+    ]).pipe(
+      map(([list, drop]) => {
+        if (drop) {
+          moveItemInArray(list, drop.previousIndex, drop.currentIndex);
+        }
+        return list;
+      }),
+      shareReplay({ refCount: true, bufferSize: 1 }),
+    );
+    this.transforms$ = this.badgeList$.pipe(
+      map((list) => {
+        const size = list.length;
+        return list.map((_, idx) => {
+          const translateY = idx < size - 1 ? (this.halfHeaderHeight * idx).toString() + 'px' : '0px';
+          const translateZ = (-5 * (size - idx - 1) + 1).toString() + 'px';
+          return `translateZ(${translateZ}) translateY(${translateY})`;
+        });
       })
     );
+  }
+
+  drop(event: CdkDragDrop<Badge>) {
+    this.drop$.next(event);
   }
 }
